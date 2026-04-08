b97f28a7f54698b6f7bb595f55d749271af70b54
Afomia <afomialink@gmail.com>
2026-04-08 22:30:05 +0300

    CI actions fix-version 3

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="traffic-sign-detection",
+    version="0.1.0",
+    description="YOLOv8n traffic sign detection on TT100K",
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
